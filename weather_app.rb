e1593ac4161a22187db540bb51342d91ab7a6c76
Kiel Arneson <kiel.arneson@gmail.com>
2021-08-20 18:10:32 -0500

    5 day forecast with basic functionality

--- weather_app.rb
+++ weather_app.rb
@@ -1,26 +1,48 @@
 require "http"
 
-response = HTTP.get("https://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
+#{ENV["OPEN_WEATHER_API_KEY"]}
+
+response = HTTP.get("https://api.openweathermap.org/data/2.5/forecast?q=chicago&units=imperial&appid=")
 weather_data = JSON.parse(response.body)
 
-five_day = []
+# pp weather_data["list"][0]
+
+temp_array = []
+real_feel_array = []
+
+puts "YOUR 5 DAY FORECAST FOR CHICAGO:"
+puts ""
 
 index = 0
-while index < 33
-  five_day << weather_data["list"][index]["main"]["temp"]
+while index < weather_data["list"].length
+  date = weather_data["list"][index]["dt_txt"].slice(0..9)
+  time = weather_data["list"][index]["dt_txt"].slice(11..)
+  temp = weather_data["list"][index]["main"]["temp"]
+  feels_like = weather_data["list"][index]["main"]["feels_like"]
+  humidity = weather_data["list"][index]["main"]["humidity"]
+
+  temp_array << weather_data["list"][index]["main"]["temp"]
+  real_feel_array << weather_data["list"][index]["main"]["feels_like"]
+
+  puts "On #{date} at #{time}, the temperature in Chicago will be #{temp} degrees with #{humidity}% humidity."
+  puts "The real-feel temperature will be #{feels_like} degrees."
+  puts ""
+
   index += 8
 end
 
-p five_day
+index = 0
+temp_sum = 0
+real_feel_sum = 0
 
-puts "Please enter a day 1 - 5:"
-day1 = gets.chomp.to_i - 1
+while index < temp_array.length
+  temp_sum += temp_array[index]
+  real_feel_sum += real_feel_array[index]
+  index += 1
 
-puts "Please enter a day 1 - 5:"
-day2 = gets.chomp.to_i - 1
-
-if five_day[day2] > five_day[day1]
-  puts "Day #{day2} is #{five_day[day2] - five_day[day1]} hotter than day #{day1}."
-else
-  puts "Day #{day1} is #{five_day[day1] - five_day[day2]} hotter than day #{day2}."
+  temp_average = temp_sum / temp_array.length
+  real_feel_average = real_feel_sum / real_feel_array.length
 end
+
+puts "The average temperature for the next 5 days is #{temp_average} degrees,"
+puts "with a real-feel average of #{real_feel_average} degress."
